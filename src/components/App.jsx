a7b6ebd69bd63337894cc30d6b1d088e1b54ee90
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-06-08 16:38:55 +0300

    phonebooke finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,59 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-export class App extends React.Component {
-  state = {
-    contacts: [
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    ]
+  );
+
+  const [filter, setFilter] = useState('');
+  const [inputName, setInputName] = useState('');
+  const [inputNumber, setInputNumber] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleInputChange = ({ target }) => {
+    switch (target.name) {
+      case 'name':
+        setInputName(target.value);
+        break;
+      case 'number':
+        setInputNumber(target.value);
+        break;
+      default:
+        return;
+    }
   };
 
-  componentDidMount() {
-    const contanctsLS = JSON.parse(localStorage.getItem('contacts'));
-    if (contanctsLS) {
-      this.setState({ contacts: contanctsLS });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  handleInputChange = ({ target }) => {
-    this.setState({ [target.name]: target.value });
-  };
-
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-    const { name, number, contacts } = this.state;
+    // const { name, number, contacts } = this.state;
 
-    if (contacts.find(contact => contact.name === name)) {
-      alert(`${name} is already in your phonebook`);
-    } else if (contacts.find(contact => contact.number === number)) {
+    if (contacts.find(contact => contact.name === inputName)) {
+      alert(`${inputName} is already in your phonebook`);
+    } else if (contacts.find(contact => contact.number === inputNumber)) {
       alert('This number exist in your phonebook');
     } else {
-      this.setState(prevState => ({
-        contacts: [
-          ...prevState.contacts,
-          this.createContact({
-            name: name,
-            number: number,
-          }),
-        ],
-      }));
+      setContacts([
+        ...contacts,
+        createContact({ name: inputName, number: inputNumber }),
+      ]);
     }
 
     evt.currentTarget.reset();
   };
 
-  createContact = data => {
+  const createContact = data => {
     const newContact = {
       ...data,
       id: nanoid(),
@@ -64,50 +61,44 @@ export class App extends React.Component {
     return newContact;
   };
 
-  handleSearchChange = () => {
-    const { contacts, filter } = this.state;
+  const handleSearchChange = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  handleFilter = evt => {
-    this.setState({ filter: evt.currentTarget.value });
+  const handleFilter = evt => {
+    setFilter(evt.target.value);
   };
 
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleDeleteContact = id => {
+    setContacts(() => contacts.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const { state, handleInputChange, handleSubmit, handleFilter } = this;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm
-          handleInputChange={handleInputChange}
-          handleSubmit={handleSubmit}
-        />
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
 
-        <h2>Contacts</h2>
-        <Filter option={state.filter} handleFilter={handleFilter} />
-        <ContactList
-          handleDeleteContact={this.handleDeleteContact}
-          options={this.handleSearchChange()}
-        />
-      </div>
-    );
-  }
-}
+      <h2>Contacts</h2>
+      <Filter option={filter} handleFilter={handleFilter} />
+      <ContactList
+        handleDeleteContact={handleDeleteContact}
+        options={handleSearchChange()}
+      />
+    </div>
+  );
+};
